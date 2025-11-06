4e26d5dac3f61a734fd85f0a0a49729e3dcab39e
Qilqilll <ASUS@LAPTOP-3HJQCS6H>
2025-11-06 17:24:33 +0700

    main activity

--- app/src/main/java/com/example/praktikum6/MainActivity.kt
+++ app/src/main/java/com/example/praktikum6/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.praktikum6
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,37 +12,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.praktikum5.DataApp
 import com.example.praktikum6.ui.theme.Praktikum6Theme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             Praktikum6Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold (modifier = Modifier.fillMaxSize()) {
+                    DataApp(modifier = Modifier)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Praktikum6Theme {
-        Greeting("Android")
     }
 }
