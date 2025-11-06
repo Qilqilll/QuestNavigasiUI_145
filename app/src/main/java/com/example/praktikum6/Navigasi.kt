cd59859608dbad4005db9bb8743d96a5ede16664
Qilqilll <ASUS@LAPTOP-3HJQCS6H>
2025-11-06 17:16:24 +0700

    inclusive

--- app/src/main/java/com/example/praktikum6/Navigasi.kt
+++ app/src/main/java/com/example/praktikum6/Navigasi.kt
@@ -48,3 +48,6 @@ fun DataApp(
 private fun cancelAndBackToFormulirku(
     navController: NavHostController
 ) {
+    navController.popBackStack(Navigasi.Formulirku.name,
+        inclusive = false)
+}
